4d3563ad0f88e9a02b833875fe433bb72f779095
Aidan3106 <72885130+Aidan3106@users.noreply.github.com>
2021-01-29 19:31:06 -0500

    To start on player movement.

---
+++ GGJ 8-Bit U/Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rd;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input
+    }
+
+    void FixedUpdate() 
+    {
+        // Pl
+    }
+
+}
